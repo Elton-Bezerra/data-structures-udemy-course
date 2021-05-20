07d080551de15add70d0ecf8a50873965b354d43
Elton Bezerra <elton.cin@hotmail.com>
2021-05-20 13:54:56 -0300

    Hashtables e linear probing

---
+++ src/br/com/elton/hashtables/Main.java
@@ -0,0 +1,27 @@
+package br.com.elton.hashtables;
+
+import br.com.elton.models.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+        
+        SimpleHashtable ht = new SimpleHashtable();
+        
+        ht.put("Jones", janeJones);
+        ht.put("Doe", johnDoe);
+        ht.put("Wilson", mikeWilson);
+        ht.put("Smith", marySmith);
+        
+        ht.printHashtable();
+        
+        System.out.println("Retrieve value for key 'Jones': " + ht.get("Jones"));
+
+        System.out.println("Retrieve value for key 'Smith': " + ht.get("Smith"));
+	}
+}
---
+++ src/br/com/elton/hashtables/SimpleHashtable.java
@@ -0,0 +1,97 @@
+package br.com.elton.hashtables;
+
+import br.com.elton.models.Employee;
+
+public class SimpleHashtable {
+
+	private StoredEmployee[] hashtable;
+	
+	public SimpleHashtable() {
+		hashtable = new StoredEmployee[10];
+	}
+	
+	public void put (String key, Employee employee) {
+		int hashedKey = hashKey(key);
+		
+		if(occupied(hashedKey)) {
+			int stopIndex = hashedKey;
+			
+			
+			if(hashedKey == hashtable.length - 1) {
+				hashedKey = 0;
+			} else {
+				hashedKey++;
+			}
+			
+			while(occupied(hashedKey) && hashedKey != stopIndex) {
+				hashedKey = (hashedKey + 1) % hashtable.length;
+			}
+			
+		}
+		
+		if(occupied(hashedKey)) {
+			System.out.println("Position already taken by other employee [" + hashedKey + "].");
+			return;
+		} 
+		
+		hashtable[hashedKey] = new StoredEmployee(key, employee);
+	}
+	
+	public Employee get(String key) {
+		int hashedKey = findKey(key);
+		
+		if(hashedKey == -1) {
+			return null;
+		}
+		
+		return hashtable[hashedKey].employee;
+	}
+	
+	private int hashKey(String key) {
+		return key.length() % hashtable.length;
+	}
+	
+	public void printHashtable() {
+		for(int i = 0; i < hashtable.length; i++) {
+			if(hashtable[i] == null) {
+				System.out.println("empty");
+			} else {
+				System.out.println("Position " + i + ": " + hashtable[i].employee);
+			}
+		}
+	}
+	
+	private boolean occupied(int index) {
+		return hashtable[index] != null;
+	}
+	
+	private int findKey(String key) {
+		int hashedKey = hashKey(key);
+		
+		//se a chave original está gravada no primeiro endereço encontrado para o hash dela, é só retornar esse hash
+		if(hashtable[hashedKey] != null && hashtable[hashedKey].key.equals(key)) {
+			return hashedKey;
+		} 
+		
+		// Caso contrário, fazer linear probing até achar o hash(index) com a chave original igual a chave do parâmetro
+		int stopIndex = hashedKey;
+		
+		
+		if(hashedKey == hashtable.length - 1) {
+			hashedKey = 0;
+		} else {
+			hashedKey++;
+		}
+		
+		while(hashedKey != stopIndex && hashtable[hashedKey] != null && !hashtable[hashedKey].key.equals(key)) {
+			hashedKey = (hashedKey + 1) % hashtable.length;
+		}
+			
+		if(stopIndex == hashedKey) {
+			return -1;
+		} else {
+			return hashedKey;
+		}
+		
+	}
+}
